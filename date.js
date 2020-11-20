5b8cdfc234bbee329c5ac606871024ccdda0de5b
Lumenw <rmgroen89@gmail.com>
2020-11-20 21:10:31 +0100

    installed mongoose

--- date.js
+++ date.js
@@ -1,9 +1,9 @@
 //js hint esversion:6
 
 exports.getDate = function () {
-  let today = new Date();
+  const today = new Date();
 
-  let options = {
+  const options = {
     weekday: "long",
     day: "numeric",
     month: "long",
@@ -12,9 +12,9 @@ exports.getDate = function () {
   return today.toLocaleDateString("en-US", options);
 };
 exports.getDay = function () {
-  let today = new Date();
+  const today = new Date();
 
-  let options = {
+  const options = {
     weekday: "long",
   };
 
